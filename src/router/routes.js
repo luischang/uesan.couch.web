472abd852c5cb9f4b42ce36811c53affb3efee79
Sebastian_Puruguay <20100770@ue.edu.pe>
2023-06-19 16:28:42 -0500

    Diseño Login

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,10 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
-
+  {
+    path: "/login",
+    component: () => import("components/auth/LoginForm.vue"),
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
