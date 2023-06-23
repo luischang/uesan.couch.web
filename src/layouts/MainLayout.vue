7579e441b107b18c50109255b44ddd9197b8bd5a
James25Frank21 <21100654@ue.edu.pe>
2023-06-23 02:46:53 -0500

    se puso la direccion inicio

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -29,7 +29,7 @@
         <q-route-tab to="/Servicios" label="Servicios" />
 
         <q-route-tab to="/QuienesSomos" label="Quienes Somos" />
-        <q-route-tab to="/" label="Inicio" />
+        <q-route-tab to="/Inicio" label="Inicio" />
       </q-tabs>
     </q-header>
 
