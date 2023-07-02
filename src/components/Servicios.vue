ea4cbfd6daa038a2e0eded33b563eeaa933d506b
James25Frank21 <21100654@ue.edu.pe>
2023-07-02 01:06:08 -0500

    actualizacion

--- src/components/Servicios.vue
+++ src/components/Servicios.vue
@@ -1,13 +1,6 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <q-layout view="LHh Lpr lFf">
-    <q-header
-      reveal
-      elevated
-      class="bg-yellow-7 text-black"
-      height-hint="98"
-      :class="{ 'header-hidden': headerHidden }"
-    >
+    <q-header reveal elevated class="bg-yellow-7 text-black" height-hint="98">
       <q-toolbar class="">
         <q-toolbar-title
           class="titulo"
@@ -34,66 +27,20 @@
       </q-toolbar>
 
       <q-tabs align="right">
-        <q-route-tab to="/login" label="Iniciar sesión" />
+        <q-route-tab to="/login" @click="reloadPage" label="Iniciar sesión" />
         <q-route-tab to="/Register" label="Registrarse" />
         <q-route-tab to="/Servicios" label="Servicios" />
         <q-route-tab to="/Comunidad" label="Comunidad" />
         <q-route-tab to="/QuienesSomos" label="Quienes Somos" />
-
-        <q-route-tab to="/ElecCoach/:idServicio" label="prueba de sebas" />
-
-        <q-route-tab to="/planPagos" label="prueba de sebas" />
-
+        <q-route-tab to="/planPagos" label="prueba de james" />
         <q-route-tab to="/Inicio" label="Inicio" />
       </q-tabs>
     </q-header>
-    <q-page-container>
-      <button @click="redirectToRouteB">Servicio 1</button>
-    </q-page-container>
-    <q-page-container class="contenservico">
-      <div class="servicio">
+
+    <q-page-container class="conti">
+      <div class="servicioscoach">
         <h2>Servicios de Coaching</h2>
         <table>
-          <tr>
-            <td>
-              <img class="admi" src="img_Servicios/administracion.jpg" />
-              <p class="pAdmi">Coaching Administración</p>
-            </td>
-            <td>
-              <img class="cont" src="img_Servicios/contabilidad.jpg" />
-              <p class="pCont">Coaching Contabilidad</p>
-            </td>
-            <td>
-              <img
-                class="deCorp"
-                src="img_Servicios//derecho corporativo.jpg"
-              />
-              <p>Coaching Derecho Corporativo</p>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img class="Fina" src="img_Servicios/finanzas.jpg" />
-              <p>Coaching Finanzas</p>
-            </td>
-            <td>
-              <img class="logi" src="img_Servicios/logistica.jpg" />
-              <p>Coaching Logistica</p>
-            </td>
-            <td>
-              <img class="mark" src="img_Servicios/Marketing.png" />
-              <p>Coaching Marketing</p>
-            </td>
-          </tr>
-        </table>
-      </div>
-    </q-page-container>
-
-<<<<<<< HEAD
-<q-page-container class="contenservico">
-  <div class="servicio">
-    <h2>Servicios de  Coaching</h2>
-    <table>
       <tr>
         <q-td >
           <RouterLink to="/MarketingService">
@@ -135,15 +82,10 @@
         </td>
       </tr>
     </table>
-  </div>
-</q-page-container>
+      </div>
+    </q-page-container>
 
-
-
-    <q-footer reveal elevated class="bg-yellow-7  text-black">
-=======
     <q-footer reveal elevated class="bg-yellow-7 text-black">
->>>>>>> 9e190c3d9aefe104f0e8757687b48653af420a5c
       <q-toolbar>
         <q-toolbar-title style="text-align: center">
           <div>Emprende Facil &copy; 2023</div>
@@ -155,85 +97,12 @@
 
 <script>
 import { defineComponent, ref } from "vue";
-export default {
-  data() {
-    return {
-      headerHidden: false,
-    };
-  },
 
-  mounted() {
-    // Agrega un listener para ocultar el header al hacer scroll hacia abajo
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  beforeUnmount() {
-    // Elimina el listener cuando el componente se destruye
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    redirectToRouteB() {
-      var idServicio = "100"; // Aquí define el valor del parámetro que quieres pasar
-      this.$router.push({ name: "ElecCoach", params: { idServicio } });
-    },
-    handleScroll() {
-      // Si el scroll vertical es mayor a 100px, oculta el header
-      this.headerHidden = window.pageYOffset > 100;
-    },
-  },
-};
+
+
 </script>
 
 <style scoped>
-.contenservico {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: yellow;
-  height: 100vh;
-}
-
-.servicio {
-  text-align: center;
-}
-
-h2 {
-  color: black;
-}
-
-table {
-  margin-top: 2rem;
-  border-collapse: collapse;
-}
-
-td {
-  padding: 1rem;
-}
-
-img {
-  width: 150px;
-  height: 150px;
-  object-fit: cover;
-  border-radius: 50%;
-}
-
-p {
-  margin-top: 0.5rem;
-  color: black;
-  font-weight: bold;
-}
-.header-hidden {
-  transform: translateY(-100%);
-}
-.contenservico {
-  background-image: url("public/imagenPro/register.png");
-  width: 100%;
-  height: auto;
-  background-size: contain;
-  background-repeat: no-repeat;
-  background-position: initial;
-  background-attachment: fixed;
-  background-size: cover;
-}
 
 .letra2 {
   width: 30%;
@@ -249,16 +118,26 @@ p {
   margin-top: 0px;
 }
 
-.ser {
+
+.servicioscoach {
+  position: relative;
   align-items: center;
-  margin-left: 3%;
-  padding-bottom: 70px;
+  justify-content: center;
+  margin-top: 200px;
+  text-align: center;
+  bottom: 160px;
 }
-.titulo {
-  font-size: 60px;
-  margin-left: 10px;
-  margin-top: 0px;
+.conti {
+  background-image: url("public/imagenPro/register.png");
+  width: 100%;
+  height: auto;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: initial;
+  background-attachment: fixed;
+  background-size: cover;
 }
+
 .servicio h2 {
   padding: 20px;
   font-size: 40px;
@@ -309,11 +188,8 @@ p {
   margin-top: 90px;
   margin-left: 150px;
 }
-
 table {
   margin-left: 15%;
-  padding-bottom: 40px;
-
 }
 table p {
   text-align: center;
@@ -321,11 +197,17 @@ table p {
 table tr td p {
   font-size: 20px;
   margin-left: 50px;
-
 }
 table tr td img {
   margin-right: 90px;
-cursor: pointer;
+}
+p {
+  color: black;
+}
 
+.product-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
 }
 </style>
