b0a18e6ed2ec3e0a8b871b775d742d323ca9f158
CuadrosA <20100754@ue.edu.pe>
2023-07-05 15:31:51 -0500

    services-styles

--- src/pages/MarketingService.vue
+++ src/pages/MarketingService.vue
@@ -32,20 +32,18 @@
         <q-route-tab to="/Servicios" label="Servicios" />
         <q-route-tab to="/Comunidad" label="Comunidad" />
         <q-route-tab to="/QuienesSomos" label="Quienes Somos" />
-        <q-route-tab to="/planPagos" label="prueba de sebas" />
         <q-route-tab to="/Inicio" label="Inicio" />
       </q-tabs>
     </q-header>
 
-    <q-page-container class="conti">
+    <q-page-container class="mrkt-srvc-container">
       <body>
         <div class="container">
           <div class="description">
-            <h2>Coaching de Marketing</h2>
-            <h6>
-              ¡Obtén el asesoramiento que necesitas para impulsar tu estrategia
-              de marketing!
-            </h6>
+            <!-- <h2>Coaching de Marketing</h2> -->
+            <div class="title">
+              <h1>Coaching de Marketing</h1>
+            </div>
             <p>
               Ya sea que necesites mejorar tu presencia en redes sociales,
               desarrollar campañas publicitarias efectivas o aumentar la
@@ -53,12 +51,51 @@
               te brinda las herramientas y conocimientos necesarios para
               alcanzar tus objetivos.
             </p>
-            <q-btn
+            <!-- <q-btn
               class="button bg-green"
               style="margin-left: 45%;"
               label="Pedir Asesoramiento"
               @click="navigateToNextPage('1005')"
-            />
+            /> -->
+            <div class="wrapper">
+              <a class="cta" @click="navigateToNextPage('1005')">
+                <span>Pedir Asesoramiento</span>
+                <span>
+                  <svg
+                    width="66px"
+                    height="43px"
+                    viewBox="0 0 66 43"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                  >
+                    <g
+                      id="arrow"
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd"
+                    >
+                      <path
+                        class="one"
+                        d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      ></path>
+                      <path
+                        class="two"
+                        d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                        fill="#FFFFFF"
+                      ></path>
+                      <path
+                        class="three"
+                        d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                        fill="#FFFFFF"
+                      ></path>
+                    </g>
+                  </svg>
+                </span>
+              </a>
+            </div>
             <p>
               Contamos con un equipo de expertos en marketing dispuestos a
               ayudarte a tomar decisiones inteligentes y estratégicas para tu
@@ -72,7 +109,10 @@
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Marketing_Mix_4P.svg/278px-Marketing_Mix_4P.svg.png"
               alt="Marketing"
             />
-            <p>Imagen referencial de Marketing</p>
+            <h6>
+              ¡Obtén el asesoramiento que necesitas para impulsar tu estrategia
+              de marketing!
+            </h6>
           </div>
         </div>
       </body>
@@ -102,19 +142,6 @@ export default {
 </script>
 
 <style scoped>
-.description h2{
-  text-align: center;
-  padding-top: 40px;
-}
-.description h6{
-  text-align: center;
-  padding-bottom: 40px;
-}
-description p{
-  text-align:justify;
-  padding-bottom: 40px;
-  width:70px;
-}
 .letra2 {
   width: 30%;
   height: 30%;
@@ -122,37 +149,181 @@ description p{
   left: 270px;
   bottom: 260px;
 }
-
 .titulo {
   font-size: 60px;
   margin-left: 10px;
   margin-top: 0px;
 }
-ody {
-  font-family: Arial, sans-serif;
-  margin: 0;
+
+/* CONTENIDO-DEL-CONTENEDOR-INFORMACIÓN */
+.mrkt-srvc-container .container {
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  padding: 30px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.mrkt-srvc-container {
+  flex: 1;
   padding: 20px;
+  padding-top: 0;
+  font-family: system-ui, sans-serif;
+}
+
+.mrkt-srvc-container .description .title {
+  width: auto;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+}
+.mrkt-srvc-container .description .title h1 {
+  margin-top: 10px;
+  font-size: 54px;
+  text-transform: uppercase;
+  font-family: "Gambetta", serif;
+  letter-spacing: -1px;
+  transition: 700ms ease;
+  font-variation-settings: "wght" 311;
+  margin-bottom: 0.8rem;
+  color: #111;
+  outline: none;
+  text-align: center;
+  margin-bottom: none;
 }
 
-.container {
-  display: flex;
-  align-items: center;
+.mrkt-srvc-container .description .title h1:hover {
+  font-variation-settings: "wght" 582;
+  letter-spacing: 1px;
 }
-
-.description {
-  flex: 1;
+.mrkt-srvc-container .description h6 {
+  /* Agregar H2 si se cambia el título */
+  text-align: center;
+  padding-top: 0px;
+  margin-top: 14px;
+  margin-bottom: 14px;
 }
-
-.button {
+/* .mrkt-srvc-container .description h2 {
+  font-size: 54px;
+  margin-bottom: 14px;
+  width: auto;
+  height: auto;
+} */
+.mrkt-srvc-container .description p {
+  padding: 20px 0;
+  margin: 0;
+  width: auto;
+  font-size: 20px;
+  /* font-weight: bold; */
+}
+.mrkt-srvc-container .description .button {
   margin-top: 10px;
 }
 
-.image {
-  width: 200px;
+/* Diseño del boton gigante */
+@import url("https://fonts.googleapis.com/css?family=Poppins:900i");
+
+.mrkt-srvc-container .container .description .wrapper {
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+}
+
+.mrkt-srvc-container .container .description .cta {
+  display: flex;
+  padding: 1px 45px;
+  text-decoration: none;
+  font-family: "Poppins", sans-serif;
+  font-size: 40px;
+  color: white;
+  background: #6225e6;
+  transition: 1s;
+  box-shadow: 6px 6px 0 black;
+  transform: skewX(-15deg);
+}
+
+.mrkt-srvc-container .container .description .cta:focus {
+  outline: none;
+}
+
+.mrkt-srvc-container .container .description .cta:hover {
+  transition: 0.5s;
+  box-shadow: 10px 10px 0 #fbc638;
+}
+
+.mrkt-srvc-container .container .description .cta span:nth-child(2) {
+  transition: 0.5s;
+  margin-right: 0px;
+}
+
+.mrkt-srvc-container .container .description .cta:hover span:nth-child(2) {
+  transition: 0.5s;
+  margin-right: 45px;
+}
+
+.mrkt-srvc-container .container .description span {
+  transform: skewX(15deg);
+}
+
+.mrkt-srvc-container .container .description span:nth-child(2) {
+  width: 20px;
+  margin-left: 30px;
+  position: relative;
+  top: 12%;
+}
+
+/**************SVG****************/
+
+.mrkt-srvc-container .container .description path.one {
+  transition: 0.4s;
+  transform: translateX(-60%);
+}
+
+.mrkt-srvc-container .container .description path.two {
+  transition: 0.5s;
+  transform: translateX(-30%);
+}
+
+.mrkt-srvc-container .container .description .cta:hover path.three {
+  animation: color_anim 1s infinite 0.2s;
+}
+
+.mrkt-srvc-container .container .description .cta:hover path.one {
+  transform: translateX(0%);
+  animation: color_anim 1s infinite 0.6s;
+}
+
+.mrkt-srvc-container .container .description .cta:hover path.two {
+  transform: translateX(0%);
+  animation: color_anim 1s infinite 0.4s;
+}
+
+/* SVG animations */
+
+@keyframes color_anim {
+  0% {
+    fill: white;
+  }
+  50% {
+    fill: #fbc638;
+  }
+  100% {
+    fill: white;
+  }
+}
+
+/* Diseño de la columna dos (imagen + texto) */
+.mrkt-srvc-container .image {
+  width: 800px;
   margin-left: 20px;
+}
+.mrkt-srvc-container .image h6 {
+  text-align: center;
+  padding-top: 0px;
+  margin-top: 14px;
+  margin-bottom: 14px;
+  font-size: 15px;
 }
 </style>
